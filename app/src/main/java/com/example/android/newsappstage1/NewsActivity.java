32ee31c6765ce2786ef4aac090101a00e490aeb9
Bogdan <posa.bogdan@yahoo.ro>
2018-07-28 16:40:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/newsappstage1/NewsActivity.java
@@ -0,0 +1,117 @@
+package com.example.android.newsappstage1;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
+
+
+    /** URL for News data from the guardian api dataset */
+    private static final String USGS_REQUEST_URL =
+            "http://content.guardianapis.com/search?show-tags=contributor&order-by=newest&page-size=10&api-key=81f23f4d-dda5-4e83-8294-9147e4d4715d";
+    /**
+     * Constant value for the News loader ID.
+     * Will be used if multiple loaders are added.
+     */
+    private static final int NEWSFEED_LOADER_ID = 1;
+
+    /** Adapter for the list of NewsFeeds */
+    private NewsAdapter mAdapter;
+
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView newsFeedListView =  findViewById(R.id.list);
+
+        mEmptyStateTextView =  findViewById(R.id.empty_view);
+        newsFeedListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of NewsFeeds as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        newsFeedListView.setAdapter(mAdapter);
+
+        // Set an item click listener on the ListView, which sends an explicit intent to web browser
+        // to open a website with more information about the selected News.
+        newsFeedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current News that was clicked on
+                News currentNews = mAdapter.getItem(position);
+
+                // Gets the url of the selected news
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the News URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                startActivity(websiteIntent);
+            }
+        });
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(NEWSFEED_LOADER_ID, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        return new NewsLoader(this, USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "Ex- No News updates available, please check back later or try again."
+        mEmptyStateTextView.setText(R.string.no_feed);
+
+        // Clear the adapter of previous News data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link News}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (news != null && !news.isEmpty()) {
+            mAdapter.addAll(news);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+    }
+}
